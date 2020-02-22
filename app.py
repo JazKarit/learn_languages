161c564bb076cfc2f5b1b0234c31d32ca7a2d093
JazKarit <Jasi5748@colorado.edu>
2020-02-22 14:21:59 -0700

    Basic web framework

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from form import WordForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'you-will-never-guess'
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
     return render_template('index.html')
+
+@app.route('/response',methods = ['POST', 'GET'])
+def response():
+    if request.method == 'POST':
+      words = request.form
+      response = words['word1']
+
+      return render_template("index.html",return_str = response)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
