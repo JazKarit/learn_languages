f8b89faa755cb9c844a331d4771c56ce276b1309
JazKarit <Jasi5748@colorado.edu>
2020-02-22 23:35:46 -0700

    stripped down version

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 from flask import Flask,render_template,request
-from form import WordForm
+# from form import WordForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you-will-never-guess'
+# app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 @app.route('/',methods=['POST','GET'])
 def index():
-    return render_template('index.html')
+    return("hello world")
+    # return render_template('index.html')
 
-@app.route('/response',methods = ['POST', 'GET'])
-def response():
-    if request.method == 'POST':
-      words = request.form
-      response = words['word1']
+# @app.route('/response',methods = ['POST', 'GET'])
+# def response():
+#     if request.method == 'POST':
+#       words = request.form
+#       response = words['word1']
 
-      return render_template("index.html",return_str = response)
+#       return render_template("index.html",return_str = response)
 
 
 
